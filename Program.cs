25c3bdbded742f20e676a0077329370395b4bd96
AnatolyKul <84135820+AnatolyKul@users.noreply.github.com>
2021-05-20 16:57:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boat Beneteau = new Boat("Beneteau", "White", "ER-256", "100 km/h");
+            Beneteau.name("Benetaeu");
+            Beneteau.color("White");
+            Beneteau.model("ER-256");
+            Beneteau.speed("100 km/h");
+            Beneteau.sail(Beneteau);
+            Beneteau.stop(Beneteau);
+        }
+    }
+}
